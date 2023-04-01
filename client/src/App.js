d8adabeeaade66cd95b974552c6b69208e517149
Ashish Kumar <ashishkr1223@gmail.com>
2023-04-01 10:38:14 +0530

    Initial files added

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+    </>
+  );
+}
+
+export default App
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <header>
+            Hello
+        </header>
+    )
+}
+export default NavBar
